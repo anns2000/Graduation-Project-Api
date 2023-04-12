e386083c6c6663a8430a4ec6ba82a4fcbd978b7b
Mario Naiem Berous <62382441+Marioktkt136@users.noreply.github.com>
2023-04-13 00:42:08 +0200

    update
    hi

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port =process.env.port || 5001
+const port = process.env.port || 5001
 app.use(express.json())
 const mongoose = require('mongoose');
 const createError = require('http-errors');
@@ -11,38 +11,38 @@ mongoose.connect('mongodb+srv://anns2000:anas123@cluster0.lyb7wo3.mongodb.net/GP
 */
 
 mongoose.connect('mongodb://0.0.0.0:27017/gptest')
-  .then(() => console.log('Connected!'));
-  
+    .then(() => console.log('Connected!'));
+
+////////
+
+app.use('/department', require('./routes/department.route'))
+app.use('/building', require('./routes/building.route'))
+app.use('/user', require('./routes/user.route'))
+app.use('/question', require('./routes/questions.route'))
 
 
-app.use('/department',require('./routes/department.route'))
-app.use('/building',require('./routes/building.route'))
-app.use('/user',require('./routes/user.route'))
-app.use('/question',require('./routes/questions.route'))
-
-  
-app.get('/', async (req , res, next) => {
-  res.send({ message: 'Awesome it works 🐻' });
+app.get('/', async(req, res, next) => {
+    res.send({ message: 'Awesome it works 🐻' });
 });
 
 app.use((req, res, next) => {
-  next(createError(501,"this root not found"));
+    next(createError(501, "this root not found"));
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.send({
-    status: err.status || 500,
-    message: err.message,
-    isError:true,
-    data:[]
-  });
+    res.status(err.status || 500);
+    res.send({
+        status: err.status || 500,
+        message: err.message,
+        isError: true,
+        data: []
+    });
 });
 
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-process.on('unhandledRejectionsss',(err)=>{
-  console.error('unhandledRejectionsss '+err)
-  process.exit(1);
+process.on('unhandledRejectionsss', (err) => {
+    console.error('unhandledRejectionsss ' + err)
+    process.exit(1);
 })
