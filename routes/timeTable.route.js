e5c33a25d80f8d2073c789129fcd8dc8027e28cf
anns2000 <88550183+anns2000@users.noreply.github.com>
2023-05-04 21:03:43 +0300

    Update timeTable.route.js

--- routes/timeTable.route.js
+++ routes/timeTable.route.js
@@ -1,11 +1,13 @@
+const { auth } = require('../middelware/auth')
 const { addTimeTable, getTimeTable, deleteTimeTable } = require('../services/timetable.services')
 
 const Route=require('express').Router()
 
 //building 
-Route.post('/',addTimeTable)
-Route.get('/',getTimeTable)
-Route.delete('/',deleteTimeTable)
+
+Route.post('/',auth,addTimeTable)
+Route.get('/',auth,getTimeTable)
+Route.delete('/',auth,deleteTimeTable)
 
 
 
